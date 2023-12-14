b36efbe135541f6e8cc6983553e2cf3b386194ce
Nyx <nyx@Nyxs-MacBook-Air.local>
2023-12-14 14:35:52 +0100

    first commit to wook

---
+++ app/lib/db-data.js
@@ -0,0 +1,94 @@
+const users = [
+  {
+    id: '410544b2-4001-4271-9855-fec4b6a6442a',
+    name: 'User1',
+    email: 'user1@nextmail.com',
+    password: '123456',
+  },
+  {
+    id: '62018bf3-8651-4e01-837f-33c7800d9e4a',
+    name: 'User2',
+    email: 'user2@nextmail.com',
+    password: 'password123',
+  },
+];
+
+const authors = [
+  {
+    id: '1',
+    name: 'Rick Riordan',
+    image_url: '../../public/rick.jpg',
+  },
+  {
+    id: '2',
+    name: 'J.C Cervantes',
+    image_url: '../../public/cervantes.jpeg',
+  },
+  {
+    id: '3',
+    name: 'Roshani Chokshi',
+    image_url: '../../public//roshani.jpg',
+  },
+  {
+    id: '4',
+    name: 'Graci Kim',
+    image_url: '../../public/kim.jpeg',
+  },
+  {
+    id: '5',
+    name: 'Yoon Ha Lee',
+    image_url: '../../public/lee.jpeg',
+  },
+  {
+    id: '6',
+    name: 'Teilor Kay Mejia',
+    image_url: '../../public/mejia.jpg',
+  },
+  {
+    id: '7',
+    name: 'Sarwat Chadda',
+    image_url: '../../public/chadda.jpeg',
+  },
+  {
+    id: '8',
+    name: 'Lori M. Lee',
+    image_url: '../../public/lori.jpeg',
+  },
+];
+
+const books = [
+  {
+    author_id: authors[0].id,
+    title: 'The Kane Chronicles 1',
+    type: 'paperback',
+    status: 'read',
+  },
+  {
+    author_id: authors[1].id,
+    title: 'Storm Runner',
+    type: 'paperback',
+    status: 'read',
+  },
+];
+
+const booksRead = [
+  { month: 'Jan', books: 6 },
+  { month: 'Feb', books: 5 },
+  { month: 'Mar', books: 4 },
+  { month: 'Apr', books: 7 },
+  { month: 'May', books: 9 },
+  { month: 'Jun', books: 11 },
+  { month: 'Jul', books: 16 },
+  { month: 'Aug', books: 19 },
+  { month: 'Sep', books: 13 },
+  { month: 'Oct', books: 10 },
+  { month: 'Nov', books: 7 },
+  { month: 'Dec', books: 3 },
+];
+
+module.exports = {
+  users,
+  authors,
+  books,
+  booksRead,
+};
--- scripts/seed.js
+++ scripts/seed.js
@@ -1,10 +1,5 @@
 const { db } = require('@vercel/postgres');
-const {
-  invoices,
-  customers,
-  revenue,
-  users,
-} = require('../app/lib/placeholder-data.js');
+const { books, authors, booksRead, users } = require('../app/lib/db-data.js');
 const bcrypt = require('bcrypt');
 
 async function seedUsers(client) {
@@ -46,53 +41,53 @@ async function seedUsers(client) {
   }
 }
 
-async function seedInvoices(client) {
+async function seedBooks(client) {
   try {
     await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
-    // Create the "invoices" table if it doesn't exist
+    // Create the "Books" table if it doesn't exist
     const createTable = await client.sql`
-    CREATE TABLE IF NOT EXISTS invoices (
+    CREATE TABLE IF NOT EXISTS books (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    customer_id UUID NOT NULL,
+    author_id UUID NOT NULL,
     amount INT NOT NULL,
     status VARCHAR(255) NOT NULL,
     date DATE NOT NULL
   );
 `;
 
-    console.log(`Created "invoices" table`);
+    console.log(`Created "books" table`);
 
-    // Insert data into the "invoices" table
-    const insertedInvoices = await Promise.all(
-      invoices.map(
-        (invoice) => client.sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
+    // Insert data into the "books" table
+    const insertedBooks = await Promise.all(
+      books.map(
+        (book) => client.sql`
+        INSERT INTO books (author_id, amount, status, date)
+        VALUES (${book.author_id}, ${book.amount}, ${book.status}, ${book.date})
         ON CONFLICT (id) DO NOTHING;
       `,
       ),
     );
 
-    console.log(`Seeded ${insertedInvoices.length} invoices`);
+    console.log(`Seeded ${insertedBooks.length} books`);
 
     return {
       createTable,
-      invoices: insertedInvoices,
+      books: insertedBooks,
     };
   } catch (error) {
-    console.error('Error seeding invoices:', error);
+    console.error('Error seeding books:', error);
     throw error;
   }
 }
 
-async function seedCustomers(client) {
+async function seedAuthors(client) {
   try {
     await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
-    // Create the "customers" table if it doesn't exist
+    // Create the "authors" table if it doesn't exist
     const createTable = await client.sql`
-      CREATE TABLE IF NOT EXISTS customers (
+      CREATE TABLE IF NOT EXISTS authors (
         id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
         email VARCHAR(255) NOT NULL,
@@ -100,62 +95,62 @@ async function seedCustomers(client) {
       );
     `;
 
-    console.log(`Created "customers" table`);
+    console.log(`Created "authors" table`);
 
-    // Insert data into the "customers" table
-    const insertedCustomers = await Promise.all(
-      customers.map(
-        (customer) => client.sql`
-        INSERT INTO customers (id, name, email, image_url)
-        VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
+    // Insert data into the "authors" table
+    const insertedAuthors = await Promise.all(
+      authors.map(
+        (author) => client.sql`
+        INSERT INTO authors (id, name, email, image_url)
+        VALUES (${author.id}, ${author.name}, ${author.email}, ${author.image_url})
         ON CONFLICT (id) DO NOTHING;
       `,
       ),
     );
 
-    console.log(`Seeded ${insertedCustomers.length} customers`);
+    console.log(`Seeded ${insertedAuthors.length} authors`);
 
     return {
       createTable,
-      customers: insertedCustomers,
+      authors: insertedAuthors,
     };
   } catch (error) {
-    console.error('Error seeding customers:', error);
+    console.error('Error seeding authors:', error);
     throw error;
   }
 }
 
-async function seedRevenue(client) {
+async function seedBooksRead(client) {
   try {
-    // Create the "revenue" table if it doesn't exist
+    // Create the "booksRead" table if it doesn't exist
     const createTable = await client.sql`
-      CREATE TABLE IF NOT EXISTS revenue (
+      CREATE TABLE IF NOT EXISTS booksRead (
         month VARCHAR(4) NOT NULL UNIQUE,
-        revenue INT NOT NULL
+        booksRead INT NOT NULL
       );
     `;
 
-    console.log(`Created "revenue" table`);
+    console.log(`Created "booksRead" table`);
 
-    // Insert data into the "revenue" table
-    const insertedRevenue = await Promise.all(
-      revenue.map(
-        (rev) => client.sql`
-        INSERT INTO revenue (month, revenue)
-        VALUES (${rev.month}, ${rev.revenue})
+    // Insert data into the "booksRead" table
+    const insertedBooksRead = await Promise.all(
+      booksRead.map(
+        (booksRead) => client.sql`
+        INSERT INTO booksRead (month, book)
+        VALUES (${booksRead.month}, ${booksRead.book})
         ON CONFLICT (month) DO NOTHING;
       `,
       ),
     );
 
-    console.log(`Seeded ${insertedRevenue.length} revenue`);
+    console.log(`Seeded ${insertedBooksRead.length} booksRead`);
 
     return {
       createTable,
-      revenue: insertedRevenue,
+      booksRead: insertedBooksRead,
     };
   } catch (error) {
-    console.error('Error seeding revenue:', error);
+    console.error('Error seeding booksRead:', error);
     throw error;
   }
 }
@@ -164,9 +159,9 @@ async function main() {
   const client = await db.connect();
 
   await seedUsers(client);
-  await seedCustomers(client);
-  await seedInvoices(client);
-  await seedRevenue(client);
+  await seedAuthors(client);
+  await seedBooks(client);
+  await seedBooksRead(client);
 
   await client.end();
 }
